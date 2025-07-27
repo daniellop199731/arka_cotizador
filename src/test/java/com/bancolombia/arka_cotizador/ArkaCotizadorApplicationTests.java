ee9bef98a1625c29fc1bb76f7d7d742812ab9e96
Daniel Lopez <daniellop199731@gmail.com>
2025-07-27 13:54:46 -0500

    Init proyect

---
+++ src/test/java/com/bancolombia/arka_cotizador/ArkaCotizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancolombia.arka_cotizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkaCotizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
