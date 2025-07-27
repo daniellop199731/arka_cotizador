3febb2210101552910bd83e037c42432ebe14c26
Daniel Lopez <daniellop199731@gmail.com>
2025-07-27 15:07:32 -0500

    Implementacion de mono en testController
    Se realiza de ejemplo sencillo de uso de Mono

--- src/main/java/com/bancolombia/arka_cotizador/infrastructure/adapter/in/web/controller/TestController.java
+++ src/main/java/com/bancolombia/arka_cotizador/infrastructure/adapter/in/web/controller/TestController.java
@@ -2,6 +2,9 @@ package com.bancolombia.arka_cotizador.infrastructure.adapter.in.web.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,23 +15,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class TestController {
 
     @GetMapping("")
-    public String test() {
-        return "Hello, this is a test endpoint (Arka_cotizador !!)!";
+    public Mono<String> test() {
+        return Mono.just("Hello, this is a test endpoint (Arka_cotizador !!)!");
     }
 
     @GetMapping("/{text}")
-    public String testPath(@PathVariable("text") String text) {
-        return "Hello, this is a test endpoint with path-parameter: " + text;
+    public Mono<String> testPath(@PathVariable("text") String text) {
+        return Mono.just("Hello, this is a test endpoint with path-parameter: " + text);
     }
     
     @GetMapping("/with-param")
-    public String testParam(@RequestParam("param") String param) {
-        return "Hello, this is a test endpoint with request-parameter: " + param;
+    public Mono<String> testParam(@RequestParam("param") String param) {
+        return Mono.just("Hello, this is a test endpoint with request-parameter: " + param);
     }
 
     @GetMapping("/with-body")
-    public String testBody(@RequestBody String param) {
-        return "Hello, this is a test endpoint with request-body: \n" + param;
+    public Mono<String> testBody(@RequestBody String param) {
+        return Mono.just("Hello, this is a test endpoint with request-body: \n" + param);
     }
 
 }
